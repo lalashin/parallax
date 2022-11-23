b24c04fe88abc2d6eaab94174b7b8db3495df616
Lala Shin <ppeanut@naver.com>
2022-11-24 02:08:15 +0900

    20221124-1

--- jquery.js
+++ jquery.js
@@ -10,4 +10,43 @@ nav.click(function(e){
     let offset = section.offset().top;     //각 컨텐츠의 오프셋 값을 할당
     //alert(offset);
     $("html, body").animate({scrollTop: offset},600,"easeOutBounce");
+    
 });
+
+$(window).scroll(function(){
+    let wScroll = $(this).scrollTop();
+
+    /*if(wScroll >= cont.eq(0).offset().top){
+        nav.removeClass("active");
+        nav.eq(0).addClass("active");
+    }
+    if(wScroll >= cont.eq(1).offset().top){
+        nav.removeClass("active");
+        nav.eq(1).addClass("active");
+    }
+    if(wScroll >= cont.eq(2).offset().top){
+        nav.removeClass("active");
+        nav.eq(2).addClass("active");
+    }
+    if(wScroll >= cont.eq(3).offset().top){
+        nav.removeClass("active");
+        nav.eq(3).addClass("active");
+    }
+    if(wScroll >= cont.eq(4).offset().top){
+        nav.removeClass("active");
+        nav.eq(4).addClass("active");
+    }
+    if(wScroll >= cont.eq(5).offset().top){
+        nav.removeClass("active");
+        nav.eq(5).addClass("active");
+    }*/
+
+    for(i = 0; i < 6; i++){
+        if(wScroll >= cont.eq(i).offset().top){
+        nav.removeClass("active");
+        nav.eq(i).addClass("active");
+        }
+        }
+
+
+})
